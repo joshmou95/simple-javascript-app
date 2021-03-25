4b306cf5a94fd2f9f64447331dde33fd97e9e76d
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-03-24 21:13:47 -0600

    Updated array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 pokemonList=[
-  {name: 'Bulbasaur', height: 7, types: ['monster', 'grass']},
-  {name: 'Ivysaur', height: 1, types: ['monster', 'grass']},
-  {name: 'Venusaur', height: 2, types: ['monster', 'grass']}
+  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
 ];
 
