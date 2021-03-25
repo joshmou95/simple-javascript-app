229036e90f31354e449d8b15e939f298ff96f9ee
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-03-24 21:10:25 -0600

    updated types in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 pokemonList=[
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Bulbasaur', height: 7, types: ['monster', 'grass']},
   {name: 'Ivysaur', height: 1, types: ['monster', 'grass']},
   {name: 'Venusaur', height: 2, types: ['monster', 'grass']}
 ];
