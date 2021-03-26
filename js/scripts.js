ca2988cc6aa557ae4bf7e208259bc05fa3649cfc
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-03-25 20:35:37 -0600

    Added for loop
    Added for loop to write out array items.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 
-pokemonList=[
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+let pokemonList = [
+  {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
   {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
   {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
 ];
 
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >=2){
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - Wow, that\'s big!</p>');
+  } else {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '</p>');
+  }
+}
