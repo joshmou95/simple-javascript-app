c2274cb93211e998d6cefe98cf815a337a1a94c8
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-03-28 14:35:35 -0600

    Created IIFE and forEach function
    Created Repository and put inside IIFE. Changed function to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+    {name: 'Charmander', height: .6, types: ['fire']},
+    {name: 'Charmeleon', height: 1.1, types: ['fire']},
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}
+  ];
 
-let pokemonList = [
-  {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-  {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-];
+  function getAll() {
+    return pokemonList;
+  }
 
-function printArrayDetails(){
-for (let i = 0; i < pokemonList.length; i++){
-  let name = pokemonList[i].name;
-  let height = ' (height: ' + pokemonList[i].height + ') ';
-    if (pokemonList[i].height >=2){
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  let name = (pokemon.name + ': ');
+  let height = (' (Height: ' + pokemon.height + ') ');
+    if (pokemon.height >= 1.5){
       document.write('<p>' + name + height + ' - Wow, that\'s big!</p>');
   } else {
       document.write('<p>' + name + height + '</p>');
   }
-}
-}
-printArrayDetails();
+})
+
