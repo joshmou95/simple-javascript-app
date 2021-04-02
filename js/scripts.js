3f5563e754830c09a1b28e04a71f864248e2d295
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-04-02 17:28:31 -0600

    Created modal to show details
    Created modal to show details of pokemon on screen. Added close button, escape, and click off modal actions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
+  
 
   function add(pokemon) {
     if (
@@ -51,16 +53,16 @@ let pokemonRepository = (function () {
     })
   }
 
-  function loadDetails(item) {
-    let url = item.detailsUrl;
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
     // fetch data from detailsUrl
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
       // Now we add image, height, and type
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
     }).catch(function (e) {
       console.error(e);
     });
@@ -68,10 +70,57 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // create close button
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h3');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: ' + pokemon.height;
+
+    let myImage = document.createElement('img');
+    myImage.classList.add('pokemon-image');
+    myImage.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(myImage);
+    modalContainer.appendChild(modal);
+    
+    modalContainer.classList.add('is-visible');
     });
   }
 
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }  
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+  
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  
   return {
     add: add,
     getAll: getAll,
